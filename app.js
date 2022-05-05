319166d86f5cd327e5503ca87a7558d42ec17d9a
Stefany Torrellas <stefany.torrellas@gmil.com>
2022-05-05 15:40:23 -0500

    Primer commit

---
+++ app.js
@@ -0,0 +1,15 @@
+// Crear tabla de multiplicar del N° 5
+
+
+const { crearArchivo } = require('./helpers/multiplicar');
+const colors = require('colors');
+const argv = require('./config/yargs');
+
+console.clear();
+
+crearArchivo( argv.b, argv.l, argv.h)
+    .then( nombreArchivo => console.log(nombreArchivo.rainbow,'creado') )
+    .catch( err => console.log());
+
+
+
---
+++ config/yargs.js
@@ -0,0 +1,32 @@
+const argv = require('yargs')
+            .option('b', {
+                alias: 'base',
+                type: 'number',
+                demandOption: true,
+                describe:'Es la base de la tabla de multiplicar'
+            })
+           
+            .option('l', {
+            alias: 'listar',
+            type: 'boolean',
+            demandOption: true,
+            default: false,
+            describe: 'Muestra la tabla en la consola'
+            })
+
+            .option('h', {
+                alias: 'hasta',
+                type: 'number',
+                demandOption: true,
+                default: 10,
+                describe: 'Muestra en tabla hasta donde va a llegar'
+                })
+            .check(( argv, options ) => {
+                if (isNaN( argv.b) ) {
+                    throw 'La base tiene que ser un número'
+                }
+                return true;
+            })
+            .argv;
+
+module.exports= argv;
